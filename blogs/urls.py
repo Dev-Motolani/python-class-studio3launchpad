f5845b3900e93995c92b7e89615e44f9366582c3
Dev-Motolani <faloyomotolani.com>
2026-05-05 18:46:51 +0100

    added forms and views for creating post

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView
 
 urlpatterns = [
     path('home', PostListView.as_view(), name='home'),
